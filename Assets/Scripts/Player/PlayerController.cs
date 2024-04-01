f0a89cf6de82b6666e6c464f509b5b1c486b99fe
Dimitrios <dimitriosbroumas@gmai.com>
2024-04-01 11:21:58 -0400

    Input basics

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,13 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public PlayerControls playerControls;
+    private InputAction movementControls;
+    private InputAction interactControls;
+    private Vector3 moveDirection  = Vector3.zero;
+    private Vector3 lastMoveDirection  = Vector3.zero;
+
+    void OnEnable()
+    {
+        movementControls = playerControls.Character.Movement; 
+        movementControls.Enable();
+
+        interactControls = playerControls.Character.Interact;
+        interactControls.Enable();
+        interactControls.performed += Interact;
+
+    }
+
+    void OnDisable()
+    {
+        movementControls.Disable();
+        interactControls.Disable();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        moveDirection = movementControls.ReadValue<Vector3>();
+        //update movement base on key inputs
+        if(moveDirection != Vector3.zero)
+        {
+            lastMoveDirection = moveDirection;
+        } 
     }
 
     // Update is called once per frame
@@ -15,4 +45,10 @@ public class PlayerController : MonoBehaviour
     {
         
     }
+
+    private void Interact(InputAction.CallbackContext context)
+    {
+        Debug.Log("lets eat");
+    }
+
 }
