f627c38a8e4507f7bcb234875435bf4087a052f7
Dimitrios <dimitriosbroumas@gmai.com>
2024-04-01 15:33:56 -0400

    Coding input controls

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -12,60 +12,47 @@ public class PlayerController : MonoBehaviour
 
     [Header("Player Controls")]
     public PlayerControls playerControls;
-    private InputAction movementControls;
-    private InputAction interactControls;
-    private Vector2 moveDirection  = Vector2.zero;
-    private Vector2 lastMoveDirection  = Vector2.zero;
+    public Vector2 viewDirection;
+    public Vector2 moveDirection;
+    public Vector2 lastMoveDirection = Vector2.zero;
 
 
     private void Awake()
     {
         playerControls = new PlayerControls();
+        
+        playerControls.Character.Movement.performed += e => moveDirection = e.ReadValue<Vector2>();
+        playerControls.Character.View.performed += e => viewDirection = e.ReadValue<Vector2>();
+        
+        playerControls.Enable();
+
+        //Get Body and set important vals
         rb = gameObject.GetComponent<Rigidbody>();
         moveSpeed = 5;
     }
 
-    void OnEnable()
-    {
-        movementControls = playerControls.Character.Movement; 
-        movementControls.Enable();
-
-        interactControls = playerControls.Character.Interact;
-        interactControls.Enable();
-        interactControls.performed += Interact;
-
-    }
-
-    void OnDisable()
-    {
-        movementControls.Disable();
-        interactControls.Disable();
-    }
-
-    // Start is called before the first frame update
     void Start()
     {
  
     }
 
-    // Update is called once per frame
     void Update()
     {
-        moveDirection = movementControls.ReadValue<Vector2>();
-        //update movement base on key inputs
-        if(moveDirection != Vector2.zero)
-        {
-            lastMoveDirection = moveDirection;
-        }
-        
-        Debug.Log("moveDirection =" + moveDirection);
-        Debug.Log("lastMoveDirection =" + lastMoveDirection);   
+        CalculateView();
+        CalculateMovement();
     }
 
-    private void FixedUpdate()
+     private void CalculateView()
     {
-        rb.velocity = new Vector2(moveDirection.x * moveSpeed, rb.velocity.y);
+        
     }
+
+    private void CalculateMovement()
+    {
+
+    }
+
+
 
     private void Interact(InputAction.CallbackContext context)
     {
