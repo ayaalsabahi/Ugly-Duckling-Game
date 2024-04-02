eb8377185395f98afe3fc01443b56ca62f537d29
Aya Al Sabahi <ayaalsabahi735@gmail.com>
2024-04-02 18:56:25 -0400

    particle emission + enemy detection working

---
+++ Assets/eatController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eatController : MonoBehaviour
+{
+    private ParticleSystem particleSystemEnemy;
+
+    private void Start()
+    {
+        particleSystemEnemy = GetComponent<ParticleSystem>();
+
+        if (particleSystemEnemy == null)
+        {
+            // If the ParticleSystem component is not found, log an error
+            Debug.Log("No ParticleSystem component found on this GameObject!");
+        }
+
+        else
+        {
+            particleSystemEnemy.Stop();
+        }
+
+    }
+
+    public void Activate()
+    {
+        Debug.Log("particle system activated");
+        particleSystemEnemy.Play();
+    }
+
+    public void Deactivate()
+    {
+        Debug.Log("Particle system deactivated");
+        particleSystemEnemy.Stop();
+    }
+}
---
+++ Assets/eatingScript.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eatingScript : MonoBehaviour
+{
+
+    /*
+     Attach this script to the 'Player', this script allows the detection of an object tagged 'edible',
+     The edible object type must have a 'eatController' script and particleSystem attached to it.
+     */
+    public float detectionDistance = 10f; // Maximum distance for detection
+    private GameObject currentEnemy = null; // Reference to the current enemy
+    private eatController eatContLocal;
+
+    private void Update()
+    {
+        if(LookEnemy() && Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Player is looking at the enemy and pressed 'Space'!");
+
+            if(currentEnemy != null){
+
+                Debug.Log("Activated the eating controller");
+                eatContLocal.Activate();                                                            //also possibly add in sound?? 
+            }
+        }
+
+        else
+        {
+            if (currentEnemy != null)
+            {
+                currentEnemy = null; // Reset the reference
+                //eatContLocal.Deactivate();
+            }
+        }
+    }
+
+
+    //This function checks to see if the player is currently looking at at enemy
+    private bool LookEnemy() {
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, detectionDistance))
+        {
+            // Check if the raycast hits an enemy
+            if (hit.collider.CompareTag("Edible"))
+            {
+                // Store the reference to the enemy
+                currentEnemy = hit.collider.gameObject;
+                if(currentEnemy == null)
+                {
+                    Debug.Log("curr enemy is null");
+                }
+                // Get the EnemyController script attached to the enemy
+                eatContLocal = currentEnemy.GetComponent<eatController>();
+                if(eatContLocal == null)
+                {
+                    Debug.Log("eatContLocal is null");
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+}
