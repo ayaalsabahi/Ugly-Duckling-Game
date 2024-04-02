a37be81510f774ed72bdb24d39fc3165b627ffe3
Dimitrios <dimitriosbroumas@gmai.com>
2024-04-02 12:11:16 -0400

    Player controls movement and interact added
    I have to add looking

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,55 +8,79 @@ public class PlayerController : MonoBehaviour
     
     [Header("Player Movement")]
     private float moveSpeed;
+    public float moveForce = 1f;
+    public float maxSpeed = 5f;
+    public Vector3 forceDirection = Vector3.zero; 
     private Rigidbody rb;
 
     [Header("Player Controls")]
     public PlayerControls playerControls;
+    private InputAction moveControls;
     public Vector2 viewDirection;
     public Vector2 moveDirection;
     public Vector2 lastMoveDirection = Vector2.zero;
+
+    [Header("Misc")]
+    public Camera playerCam;
 
 
     private void Awake()
     {
         playerControls = new PlayerControls();
         
-        playerControls.Character.Movement.performed += e => moveDirection = e.ReadValue<Vector2>();
-        playerControls.Character.View.performed += e => viewDirection = e.ReadValue<Vector2>();
+        // playerControls.Character.Movement.performed += e => moveDirection = e.ReadValue<Vector2>();
+        // playerControls.Character.View.performed += e => viewDirection = e.ReadValue<Vector2>();
         
-        playerControls.Enable();
+        // playerControls.Enable();
 
         //Get Body and set important vals
         rb = gameObject.GetComponent<Rigidbody>();
         moveSpeed = 5;
     }
 
-    void Start()
+    private void OnEnable()
     {
- 
+        playerControls.Character.Interact.started += InteractEvent;
+        moveControls = playerControls.Character.Movement;
+        playerControls.Enable();
     }
 
-    void Update()
+    private void OnDisable()
     {
-        CalculateView();
-        CalculateMovement();
+        playerControls.Character.Interact.started -= InteractEvent;
+        playerControls.Disable();
     }
 
-     private void CalculateView()
+    void FixedUpdate()
     {
-        
+        forceDirection += moveControls.ReadValue<Vector2>().x * GetCameraRight(playerCam) * moveForce;
+        forceDirection += moveControls.ReadValue<Vector2>().y * GetCameraForward(playerCam) * moveForce;
+        rb.AddForce(forceDirection, ForceMode.Impulse);
+        forceDirection = Vector3.zero;
     }
 
-    private void CalculateMovement()
+    private Vector3 GetCameraForward(Camera cam)
     {
-
+        Vector3 forward = cam.transform.forward;
+        forward.y = 0;
+        return forward.normalized; 
     }
 
+    private Vector3 GetCameraRight(Camera cam)
+    {
+        Vector3 right = cam.transform.right;
+        right.y = 0;
+        return right.normalized; 
+    }
 
-
-    private void Interact(InputAction.CallbackContext context)
+    private void InteractEvent(InputAction.CallbackContext context)
     {
         Debug.Log("lets eat");
+    }
+
+    private void Update()
+    {
+        Debug.Log(rb.velocity);
     }
 
 }
