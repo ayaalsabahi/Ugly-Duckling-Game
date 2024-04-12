8975b6679e1cfb095e2b01cb8c30358f39559c35
Dimitrios <dimitriosbroumas@gmai.com>
2024-04-12 17:14:52 -0400

    Quest fix

--- Assets/Scripts/All/Quest.cs
+++ Assets/Scripts/All/Quest.cs
@@ -6,9 +6,11 @@ using UnityEngine;
 public class Quest : ScriptableObject
 {
     public string questID;
+    public string collectibleID;
     public string description;
     public string startDialouge;
     public string endDialouge;
     public bool isComplete;
+    
 
 }
