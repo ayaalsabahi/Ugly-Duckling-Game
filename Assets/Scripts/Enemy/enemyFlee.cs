f1d74a509ef7789838eb4f121e36e0c976a66c48
Aya Al Sabahi <ayaalsabahi735@gmail.com>
2024-04-14 20:40:58 -0400

    running working

--- Assets/Scripts/Enemy/enemyFlee.cs
+++ Assets/Scripts/Enemy/enemyFlee.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI; 
 
 public class enemyFlee : MonoBehaviour
 {
@@ -13,17 +14,41 @@ public class enemyFlee : MonoBehaviour
 
 
     For now:
-        - they run up to 30s and only as long as they are at a distance away by detection distance 
+        - they run away from the player as it gets closer to them if flee mode is activated; 
      */
 
     public float detectionRadius; //this distance is for the 'eating' being visible versus not 
+    private NavMeshAgent agent;
+    public float enemyDistanceRun = 5f;
+    private bool isFlee = false;
+    public GameObject player;
+    public Material fleematerial; //temporary color change when fleeing
 
-    
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (isFlee)
+        {
+            float distance = Vector3.Distance(transform.position, player.transform.position);
+
+            if(distance < enemyDistanceRun)
+            {
+                Vector3 dirToPlayer = transform.position - player.transform.position;
+                Vector3 newPos = transform.position + dirToPlayer;
+                agent.SetDestination(newPos);
+            }
+        }
+    }
+
 
     //get all the objects that are near me and check if any of the objects nearby is the player
     public void isNear()
     {
-        Debug.Log("Is near was called");
+        
         Collider[] colliders = Physics.OverlapSphere(transform.position, detectionRadius);
 
         // Loop through all the colliders that intersect with the sphere
@@ -31,6 +56,7 @@ public class enemyFlee : MonoBehaviour
         {
             if (collider.CompareTag("Player") && !GameManager.Instance.isHidden) // as well as the object not being hidden 
             {
+                
                 fleeMode();
                 
             }
@@ -41,7 +67,10 @@ public class enemyFlee : MonoBehaviour
 
     private void fleeMode()
     {
-        Debug.Log("I am now fleeing");
+        Debug.Log("flee mode is called");
+        isFlee = true;
+        Renderer renderer = gameObject.GetComponent<Renderer>();
+        renderer.material = fleematerial; //change the color of the duck
     }
 
 
