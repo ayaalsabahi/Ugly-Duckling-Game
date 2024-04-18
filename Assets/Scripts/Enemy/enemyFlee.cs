f565e0f146fa613e03bbfac2b035a99599caefb9
Aya Al Sabahi <ayaalsabahi735@gmail.com>
2024-04-17 23:02:14 -0400

    added floating ducks

--- Assets/Scripts/Enemy/enemyFlee.cs
+++ Assets/Scripts/Enemy/enemyFlee.cs
@@ -6,7 +6,7 @@ using UnityEngine.AI;
 public class enemyFlee : MonoBehaviour
 {
     /*enemyFlee: This script detects if an enemy is within the radius of the player 
-                 as well as activating a fleeAI when it is
+                 as well as activating a fleeAI when it is otherwise, it walks around normally 
     
      Thinsg to keep in mind:
         - is it timed?
@@ -17,16 +17,28 @@ public class enemyFlee : MonoBehaviour
         - they run away from the player as it gets closer to them if flee mode is activated; 
      */
 
+    private bool isFlee;
+    public GameObject player;
+
+    //for fleeing 
     public float detectionRadius; //this distance is for the 'eating' being visible versus not 
     private NavMeshAgent agent;
     public float enemyDistanceRun = 5f;
-    private bool isFlee = false;
-    public GameObject player;
     public Material fleematerial; //temporary color change when fleeing
+
+    //not fleeing 
+    public float roamRadius = 10f;
+    public float roamTimer = 5f; //every how many seconds change positions
+    private float timer; //temporary variable
+    private Vector3 randomDestination;
 
     private void Start()
     {
+        isFlee = false;
         agent = GetComponent<NavMeshAgent>();
+        timer = roamTimer;
+        SetRandomDestination();
+
     }
 
     private void Update()
@@ -42,6 +54,15 @@ public class enemyFlee : MonoBehaviour
                 agent.SetDestination(newPos);
             }
         }
+        timer -= Time.deltaTime;
+        if (timer <= 0f) //when not in fleeing mode, roam around
+        {
+            SetRandomDestination();
+            timer = roamTimer;
+        }
+
+        Debug.Log(timer);
+        //we will use -1 as the layers for all layers to be included in the navmesh for now 
     }
 
 
@@ -87,5 +108,22 @@ public class enemyFlee : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    private void SetRandomDestination()
+    {
+        randomDestination = RandomSphere(transform.position, roamRadius, -1);
+        Debug.Log("New Destination: " + randomDestination);
+        agent.SetDestination(randomDestination);
+    }
+
+    private Vector3 RandomSphere(Vector3 origin, float dist, int layers) 
+    {
+        Vector3 dirRand = Random.insideUnitSphere * dist;
+        dirRand += origin;
+        NavMeshHit navHit;
+        NavMesh.SamplePosition(dirRand, out navHit, dist, layers); 
+        return navHit.position;
+
     }
 }
