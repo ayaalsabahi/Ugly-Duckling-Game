3d16168a9ab3c06b5d7736725c54f5f9a1e5154f
Aya Al Sabahi <ayaalsabahi735@gmail.com>
2024-04-18 16:45:30 -0400

    made terrain changes

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioClip smallBiteSound;
+    public AudioClip bigBiteSound;
+
+
+    private AudioSource smallBiteSource;
+    private AudioSource bigBiteSource; 
+
+    //singleton setup 
+    public static SoundManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); //in case we add more scenes later on things should not change
+        }
+    }
+
+    public void Start()
+    {
+        smallBiteSource = gameObject.AddComponent<AudioSource>();
+        bigBiteSource = gameObject.AddComponent<AudioSource>();
+
+        smallBiteSource.clip = smallBiteSound;
+        bigBiteSource.clip = bigBiteSound; 
+    }
+
+
+    public void smallBitePlay()
+    {
+        smallBiteSource.Play();
+    }
+
+    public void bigBitePlay()
+    {
+        bigBiteSource.Play();
+    }
+}
