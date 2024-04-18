8266de2f5327ec78838015f879e5f0de56563fc0
Aya Al Sabahi <ayaalsabahi735@gmail.com>
2024-04-18 17:16:13 -0400

    added camera

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,19 +4,35 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target;
-    public float smoothing = 10f;
-    Vector3 offset;
+    //public Transform target;
+    //public float smoothing = 10f;
+    //Vector3 offset;
 
-    private void Start()
-    {
-        offset = transform.position - target.position;
-    }
+    //private void Start()
+    //{
+    //    offset = transform.position - target.position;
+    //}
 
-    // Update is called once per frame
-    void LateUpdate()
+    //// Update is called once per frame
+    //void LateUpdate()
+    //{
+        
+    //    Vector3 targetCamPos = target.position + offset;
+    //    transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+    //}
+
+
+    //temporarily updating the camera
+    public Transform playerTransform;
+    public float smoothing = 5f;
+    public Vector3 offset = new Vector3(0f, 2f, -20f); //ffset to position the camera behind the player
+
+    private void LateUpdate()
     {
-        Vector3 targetCamPos = target.position + offset;
-        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+        if (playerTransform != null)
+        {
+            Vector3 targetPosition = playerTransform.position + offset;
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing * Time.deltaTime);
+        }
     }
 }
