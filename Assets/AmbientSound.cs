9077489e0e5951132155fa482f53965a736583d7
Aya Al Sabahi <ayaalsabahi735@gmail.com>
2024-04-24 19:39:31 -0400

    ambient sound

---
+++ Assets/AmbientSound.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbientSound : MonoBehaviour
+{
+    public AudioClip sound;
+
+
+    private AudioSource soundSource;
+    public Transform player;
+    public float minVolumeDistance;
+    public float maxVolumeDistance;
+    public float maxVolume = 1.0f;
+
+    private void Awake()
+    {
+        soundSource = gameObject.AddComponent<AudioSource>();
+        soundSource.clip = sound;
+        soundSource.minDistance = minVolumeDistance;
+        soundSource.maxDistance = maxVolumeDistance;
+        soundSource.loop = true;
+        soundSource.Play();
+    }
+
+    private void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(gameObject.transform.position, player.position);
+        //Debug.Log("dist to player" + distanceToPlayer);
+        float normalizedDistance = Mathf.Clamp01((distanceToPlayer - minVolumeDistance) / (maxVolumeDistance - minVolumeDistance));
+        float volume = Mathf.Clamp01(1.0f - Mathf.Log(normalizedDistance + 1.0f) / Mathf.Log(2.0f));
+        soundSource.volume = volume * maxVolume;
+    }
+
+
+}
---
+++ Assets/sonSound.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sonSound : MonoBehaviour
+{
+    public AudioClip soundClip; // Drag and drop your sound clip into this field in the Unity editor
+    private AudioSource audioSource;
+
+    private float timer = 0f;
+    public float interval = 3f; // Interval in seconds between each sound
+
+    public Transform player;
+    public float minVolumeDistance;
+    public float maxVolumeDistance;
+    public float maxVolume = 1.0f;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            // If there's no AudioSource component attached to this GameObject, add one
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= interval)
+        {
+            // Play the sound
+            if (soundClip != null)
+            {
+                audioSource.PlayOneShot(soundClip);
+            }
+
+            // Reset the timer
+            timer = 0f;
+        }
+
+        float distanceToPlayer = Vector3.Distance(gameObject.transform.position, player.position);
+        float normalizedDistance = Mathf.Clamp01((distanceToPlayer - minVolumeDistance) / (maxVolumeDistance - minVolumeDistance));
+        float volume = Mathf.Clamp01(1.0f - Mathf.Log(normalizedDistance + 1.0f) / Mathf.Log(2.0f));
+        audioSource.volume = volume * maxVolume;
+    }
+}
