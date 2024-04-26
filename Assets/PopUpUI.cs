1e059362e1567b0ca401e70659765d4d8e5dd1bc
Aya Al Sabahi <ayaalsabahi735@gmail.com>
2024-04-25 23:00:18 -0400

    added ending UI

--- Assets/PopUpUI.cs
+++ Assets/PopUpUI.cs
@@ -6,18 +6,21 @@ public class PopUpUI : MonoBehaviour
 {
     public Quest finalQuest;
     public GameObject finalCanvas;
-
+    private bool setBefore = false; 
     // Update is called once per frame
     void Update()
     {
-        if (finalQuest.isComplete)
+        if (finalQuest.isComplete & !setBefore)
         {
             finalCanvas.SetActive(true);
+            setBefore = true; 
         }
     }
 
     public void removeQuest()
     {
+        Debug.Log("I am being set inactive");
         finalCanvas.SetActive(false);
+        setBefore = true; 
     }
 }
