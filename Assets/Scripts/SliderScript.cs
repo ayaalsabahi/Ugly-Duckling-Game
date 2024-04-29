ddf4eee221340a0d2d9ad72039331d790f1b6ffb
idolish7 <122846207+idolish7@users.noreply.github.com>
2024-04-29 19:48:03 -0400

    Changed defaults for all variables
    Added the default values in a switch case that resets each to a *reasonable* value given the nature of the attribute

--- Assets/Scripts/SliderScript.cs
+++ Assets/Scripts/SliderScript.cs
@@ -8,6 +8,7 @@ public class SliderScript : MonoBehaviour
 {
     public GameObject playerDuck;
     private Rigidbody duckrb;
+    private PlayerController duckpc;
     public GameObject water;
 
     Slider[] sliders;
@@ -18,7 +19,6 @@ public class SliderScript : MonoBehaviour
     [SerializeField] private Slider slider4;
     [SerializeField] private Slider slider5;
     [SerializeField] private Slider slider6;
-    [SerializeField] private Slider slider7;
 
     [SerializeField] private TextMeshProUGUI slider1Text;
     [SerializeField] private TextMeshProUGUI slider2Text;
@@ -26,13 +26,17 @@ public class SliderScript : MonoBehaviour
     [SerializeField] private TextMeshProUGUI slider4Text;
     [SerializeField] private TextMeshProUGUI slider5Text;
     [SerializeField] private TextMeshProUGUI slider6Text;
-    [SerializeField] private TextMeshProUGUI slider7Text;
 
     private void Start()
     {
+        // Prepare water and duck components
         duckrb = playerDuck.GetComponent<Rigidbody>();
+        duckpc = playerDuck.GetComponent<PlayerController>();
 
         sliders = FindObjectsOfType<Slider>();
+
+        // Set default values
+        btn_ResetSliders();
 
         // Static Friction
         slider1.onValueChanged.AddListener((v) => {
@@ -53,42 +57,64 @@ public class SliderScript : MonoBehaviour
         // Drag
         slider3.onValueChanged.AddListener((v) => {
 
-            slider4Text.text = v.ToString("0.00");
-
+            slider3Text.text = v.ToString("0.00");
+            duckrb.drag = v;
         });
 
         // Angular Drag
-        slider3.onValueChanged.AddListener((v) => {
+        slider4.onValueChanged.AddListener((v) => {
 
             slider4Text.text = v.ToString("0.00");
-
+            duckrb.angularDrag = v;
         });
 
         // Mass
-        slider3.onValueChanged.AddListener((v) => {
+        slider5.onValueChanged.AddListener((v) => {
 
-            slider4Text.text = v.ToString("0.00");
-
-        });
-
-        // Move Speed
-        slider3.onValueChanged.AddListener((v) => {
-
-            slider4Text.text = v.ToString("0.00");
-
+            slider5Text.text = v.ToString("0.00");
+            duckrb.mass = v;
         });
 
         // Move Force
-        slider3.onValueChanged.AddListener((v) => {
+        slider6.onValueChanged.AddListener((v) => {
 
-            slider4Text.text = v.ToString("0.00");
-
+            slider6Text.text = v.ToString("0.00");
+            duckpc.moveForce = v;
         });
+
     }
 
     public void btn_ResetSliders()
     {
         for (int i = 0; i < sliders.Length; i++)
-            sliders[i].value = 0;
+        {
+            // Setting the default values for all variables
+            switch (sliders[i].name)
+            {
+                case "FrictionSlider":
+                    sliders[i].value = 0.0f;
+                    break;
+                case "DynamicSlider":
+                    sliders[i].value = 0.0f;
+                    break;
+                case "DragSlider":
+                    sliders[i].value = 3.0f;
+                    break;
+                case "AngularDragSlider":
+                    sliders[i].value = 0.05f;
+                    break;
+                case "MassSlider":
+                    sliders[i].value = 1.0f;
+                    break;
+                case "MoveForceSlider":
+                    sliders[i].value = 3.0f;
+                    break;
+                default:
+                    // Handle default case
+                    break;
+            }
+            //Debug.Log(sliders[i].name);
+        }
+      
     }
 }
